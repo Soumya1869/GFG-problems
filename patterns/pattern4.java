726d19ce49063fca3eaac2a7e406020980289ca7
SOUMYA SINGH <103172720+Soumya1869@users.noreply.github.com>
2023-06-29 00:40:25 +0530

    Add files via upload

---
+++ patterns/pattern4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class pattern4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+input: 5
+output:
+
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+ */
